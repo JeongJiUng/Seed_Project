56f0ad07e57a2a9e9cba475c62ae7cb4449c1151
Jeong ji ung <anarkia5836@gmail.com>
2019-11-16 21:28:12 +0900

    Memory Pool Class
    개발 완료.

--- Leviathan/lev_tcmalloc.cpp
+++ Leviathan/lev_tcmalloc.cpp
@@ -1,1 +1,32 @@
 #include "lev.h"
+// https://github.com/gperftools/gperftools
+
+lev::cTCMalloc::cTCMalloc()
+{
+}
+
+lev::cTCMalloc::~cTCMalloc()
+{
+}
+
+void* lev::cTCMalloc::alloc(const size_t _size)
+{
+	return tc_new(_size);
+}
+
+void* lev::cTCMalloc::alloc_array(const size_t _size)
+{
+	return tc_newarray(_size);
+}
+
+void lev::cTCMalloc::dealloc(void* _p)
+{
+	tc_delete(_p);
+	_p						= NULL;
+}
+
+void lev::cTCMalloc::dealloc_array(void* _p)
+{
+	tc_deletearray(_p);
+	_p						= NULL;
+}
--- Leviathan/lev_tcmalloc.h
+++ Leviathan/lev_tcmalloc.h
@@ -1,8 +1,16 @@
 #pragma once
 namespace lev
 {
-class cTCMalloc
+class cTCMalloc : public cMemoryPool
 {
+public:
+	cTCMalloc();
+	virtual ~cTCMalloc();
 
+public:
+	void*					alloc(const size_t _size);
+	void*					alloc_array(const size_t _size);
+	void					dealloc(void* _p);
+	void					dealloc_array(void* _p);
 };
 }

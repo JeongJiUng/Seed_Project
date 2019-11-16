79960e28a48ef136f62db50b7e0a0359a0d2fcf4
Jeong ji ung <anarkia5836@gmail.com>
2019-11-16 22:41:16 +0900

    cStream Class
    개발 완료.

---
+++ Leviathan/lev_stream.cpp
@@ -0,0 +1,88 @@
+#include "lev.h"
+
+lev::cStream::cStream()
+{
+	m_buffer_pointer		= NULL;
+	m_length				= 0;
+}
+
+lev::cStream::~cStream()
+{
+
+}
+
+bool lev::cStream::set_buffer(char* _buffer)
+{
+	if (!_buffer)
+		return FAIL;
+
+	m_buffer_pointer		= _buffer;
+	m_length				= 0;
+
+	return false;
+}
+
+size_t lev::cStream::get_length()
+{
+	return m_length;
+}
+
+bool lev::cStream::read_int32(int* _data, size_t _length)
+{
+	return read(_data, _length);
+}
+
+bool lev::cStream::read_int64(int64_t* _data, size_t _length)
+{
+	return read(_data, _length);
+}
+
+bool lev::cStream::read_char(char* _data, size_t _length)
+{
+	return read(_data, _length);
+}
+
+bool lev::cStream::read_float(float* _data, size_t _length)
+{
+	return read(_data, _length);
+}
+
+bool lev::cStream::read_ushort(ushort* _data, size_t _length)
+{
+	return read(_data, _length);
+}
+
+bool lev::cStream::read_short(short* _data, size_t _length)
+{
+	return read(_data, _length);
+}
+
+bool lev::cStream::write_int32(int _data, size_t _length)
+{
+	return write(_data, _length);
+}
+
+bool lev::cStream::write_int64(int64_t _data, size_t _length)
+{
+	return write(_data, _length);
+}
+
+bool lev::cStream::write_char(char _data, size_t _length)
+{
+	return write(_data, _length);
+}
+
+bool lev::cStream::write_float(float _data, size_t _length)
+{
+	return write(_data, _length);
+}
+
+bool lev::cStream::write_ushort(ushort _data, size_t _length)
+{
+	return write(_data, _length);
+}
+
+bool lev::cStream::write_short(short _data, size_t _length)
+{
+	return write(_data, _length);
+}

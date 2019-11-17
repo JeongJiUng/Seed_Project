a82f1fd3e8116e9dcd2f2cd12e78a3c07044aa4e
Jeong ji ung <anarkia5836@gmail.com>
2019-11-18 00:49:40 +0900

    Thread Lock 구현.
    lock, unlock으로 생기는 스트레스를 줄이기 위해 Lock이 필요한 함수 내부에서 cLock 객체를 생성하여 생성자, 소멸자를 통해 자동으로 lock, unlock할 수 있도록 구현.

--- Leviathan/lev.h
+++ Leviathan/lev.h
@@ -54,6 +54,8 @@ using namespace lev;
 #include "lev_tcmalloc.h"
 #include "lev_allocator.h"
 
+#include "lev_mutex_lock.h"
+#include "lev_multi_thread_sync.h"
 #include "lev_stream.h"
 
 #include "lev_close_socket.h"
